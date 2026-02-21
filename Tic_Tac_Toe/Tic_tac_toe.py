7d4b9b64626581761f7cd15c6c375ea4ea1c8a68
Aryan Ghodke <aryanghodke77@gmail.com>
2026-02-22 01:22:08 +0530

    UI and logic layer rectification

--- Tic_Tac_Toe/Tic_tac_toe.py
+++ Tic_Tac_Toe/Tic_tac_toe.py
@@ -1,22 +1,20 @@
 
-def guide_show():
+def show_board(board):
+    return (
+        f"\n"
+        f" {board[0]} | {board[1]} | {board[2]} \n"
+        f"---+---+---\n"
+        f" {board[3]} | {board[4]} | {board[5]} \n"
+        f"---+---+---\n"
+        f" {board[6]} | {board[7]} | {board[8]} \n"
+    )
+
+def guide_show_board():
     board = [" 1", "2", "3",
             " 4", "5", "6",
             " 7", "8", "9"]
 
-    print(board[0], "|", board[1], "|", board[2])
-    print("-----------")
-    print(board[3], "|", board[4], "|", board[5])
-    print("-----------")
-    print(board[6], "|", board[7], "|", board[8])
-
-def show(board):
-
-    print(board[0], " |", board[1], "|", board[2])
-    print("-----------")
-    print(board[3], " |", board[4], "|", board[5])
-    print("-----------")
-    print(board[6], " |", board[7], "|", board[8])
+    return show_board(board)
 
 def player_selection():
     player1 = input("\nChoose 'X' or 'O': ").upper()
@@ -24,20 +22,25 @@ def player_selection():
         if (player1 == "X" or player1 == "O"):
             break
         else:
-            print("Invalid choise. Please choose 'X' or 'O'.")
+            print("Invalid choice. Please choose 'X' or 'O'.")
             player1 = input("Choose 'X' or 'O': ").upper()
             
     player2 = "O" if player1 == "X" else "X"
 
     return player1, player2
 
-def player_move(board,player):
-    move = int(input(f"\nPlayer {player} choose your move (1-9): "))
-    while not (1 <= move <= 9) or (board[move - 1] != " "):
-        move = int(input(f"Invalide choice. Player {player} choose your move (1-9): "))
+def player_move(board, player):
+    while True:
+        try:
+            move = int(input(f"\nPlayer {player} choose your move (1-9): "))
+            if 1 <= move <= 9 and board[move - 1] == " ":
+                break
+            else:
+                print("Invalid choice.")
+        except ValueError:
+            print("Please enter a number from 1 to 9.")
 
     board[move - 1] = player
-    show(board)
 
 def check_game(board):
     checklist = [[0,3,6],[1,4,7],[2,5,8], 
@@ -54,13 +57,16 @@ def check_game(board):
         return 1
 
     return 0
+
+def play_again_answer(answer: str) -> bool:
+    return answer.lower().startswith("y")
     
-def game():
+while(True):
     print("------ This is TIC TAC TOE Game ------\n")
-    guide_show()
+    print(guide_show_board())
     print("\n")
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    show(board)
+    board = [" "] * 9
+    print(show_board(board))
     
     player1, player2 = player_selection()
     current_player = player1
@@ -68,15 +74,14 @@ def game():
     while(True):
     
         player_move(board,current_player)
+        print("\n",show_board(board))
         if check_game(board):
             break
         current_player = player2 if current_player == player1 else player1
 
-    a = input("Want to play again (y/n) : ")
-
-    if a == "y":
-        game()
+    answer = input("\nDo you want to play again? (y/n): ")
+    if play_again_answer(answer):
+        continue
     else:
         print("\n ----- Have a nice day!!! -----")
-
-game()
+        break
