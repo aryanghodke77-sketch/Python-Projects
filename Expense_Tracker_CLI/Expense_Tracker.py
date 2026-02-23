04c12bdf7f6486eec2842cf7baf4d6c1b24bdd5a
Aryan Ghodke <aryanghodke77@gmail.com>
2026-02-24 00:42:18 +0530

    Created Id generator function

--- Expense_Tracker_CLI/Expense_Tracker.py
+++ Expense_Tracker_CLI/Expense_Tracker.py
@@ -7,4 +7,20 @@ def data_table(row,col):
 
     return data_list
 
+def id_generator(row,col):
+     id_list = [[[] for _ in range(row)] for _ in range(col)]
 
+     for j in range(col):
+        for i in range(row):
+            id_list[j][0] = (j+1)
+     
+     return id_list
+
+row = 3
+col = 3
+
+h = data_table(row,col)
+print(h)
+
+l = id_generator(row,col)
+print(l)  
