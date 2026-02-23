32043e597149b34b5225ba7b3a066613668c9549
Aryan Ghodke <aryanghodke77@gmail.com>
2026-02-24 00:55:07 +0530

    Found method to integrate id in DataTable

--- Expense_Tracker_CLI/Expense_Tracker.py
+++ Expense_Tracker_CLI/Expense_Tracker.py
@@ -1,26 +1,29 @@
 def data_table(row,col):
     data_list = [[[]for _ in range(row)] for _ in range(col)]
 
-    for j in range(col):
-        for i in range(row):
-            data_list[j][i]
+    for i in range(col):
+        for j in range(row):
+            data_list[i][j] = 0
 
     return data_list
 
 def id_generator(row,col):
      id_list = [[[] for _ in range(row)] for _ in range(col)]
 
-     for j in range(col):
-        for i in range(row):
-            id_list[j][0] = (j+1)
+     for i in range(col):
+        for j in range(row):
+            id_list[i][0] = (i+1)
      
      return id_list
 
 row = 3
 col = 3
 
-h = data_table(row,col)
-print(h)
+list1 = data_table(row,col)
+list2 = id_generator(row,col)
 
-l = id_generator(row,col)
-print(l)  
+for i in range(col):
+    for j in range(row):
+        list1[i][0] = list2[i][0]
+
+print(list1)
